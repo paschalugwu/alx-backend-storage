b0e290023e871e0e13e97a9836ff7b3c5bdac80b
paschalugwu <ugwupaschal@gmail.com>
2024-06-26 15:48:28 +0100

    feat(nosql): complete NoSQL project tasks with MongoDB scripts and Python functions
    - Implemented MongoDB shell scripts for:
      - Listing all databases
      - Creating or using a database
      - Inserting documents
      - Listing all documents
      - Listing documents by match
      - Counting documents
      - Updating documents
      - Deleting documents by match
    - Developed Python functions for MongoDB operations:
      - Listing all documents in a collection
      - Inserting documents with dynamic attributes
      - Updating topics in documents based on name
      - Retrieving schools by specific topic
      - Generating stats for Nginx logs in MongoDB
    - Included sample data and usage examples
    - Updated README with project overview and instructions

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,54 +1,45 @@
 #!/usr/bin/env python3
-"""
-Aggregation operations
-"""
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
-from collections import OrderedDict
-from typing import Tuple
 
+if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-def get_nginx_stats() -> Tuple:
-    """
-    Queries nginx collection for specific data
-    - Returns:
-        - count of all documents
-        - count of each method in the collection
-        - count of each GET calls to /status path
-        - count of top 10 visited ips
-    """
-    client: MongoClient = MongoClient()
-    db = client.logs
-    collection = db.nginx
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    method_stats = []
-    for method in methods:
-        method_count = collection.count_documents({'method': method})
-        method_stats.append({'method': method, 'count': method_count})
-    doc_count = collection.estimated_document_count()
-    status_path_stats = collection.count_documents({'method': 'GET',
-                                                    'path': '/status'})
-    pipeline = [{'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
-                {'$sort': OrderedDict([('count', -1)])},
-                {'$limit': 10}]
-    top_ips = collection.aggregate(pipeline)
-    client.close()
-    return doc_count, method_stats, status_path_stats, top_ips
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
 
-
-def print_nginx_stats() -> None:
-    """
-    Prints stats from nginx query
-    """
-    doc_count, method_stats, status_path_stats, top_ips = get_nginx_stats()
-    print(f'{doc_count} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
-    for method in method_stats:
-        print(f'\tmethod {method.get("method")}: {method.get("count")}')
-    print(f'{status_path_stats} status check')
-    print('IPs:')
-    for ip in top_ips:
-        print(f'\t{ip.get("_id")}: {ip.get("count")}')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
 
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
 
-if __name__ == '__main__':
-    print_nginx_stats()
+    print(f'{status_check} status check')
+
+    top_ips = nginx_collection.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+
+    print("IPs:")
+    for top_ip in top_ips:
+        ip = top_ip.get("ip")
+        count = top_ip.get("count")
+        print(f'\t{ip}: {count}')
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,45 +1,23 @@
 #!/usr/bin/env python3
-"""
-Aggregation operations
-"""
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
-from typing import Tuple
 
+if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-def get_nginx_stats() -> Tuple:
-    """
-    Queries nginx collection for specific data
-    - Returns:
-        - count of all documents
-        - count of each method in the collection
-        - count of each GET calls to /status path
-    """
-    client: MongoClient = MongoClient()
-    db = client.logs
-    collection = db.nginx
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    method_stats = []
-    for method in methods:
-        method_count = collection.count_documents({'method': method})
-        method_stats.append({'method': method, 'count': method_count})
-    doc_count = collection.estimated_document_count()
-    status_path_stats = collection.count_documents({'method': 'GET',
-                                                    'path': '/status'})
-    client.close()
-    return doc_count, method_stats, status_path_stats
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
 
-
-def print_nginx_stats() -> None:
-    """
-    Prints stats from nginx query
-    """
-    doc_count, method_stats, status_path_stats = get_nginx_stats()
-    print(f'{doc_count} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
-    for method in method_stats:
-        print(f'\tmethod {method.get("method")}: {method.get("count")}')
-    print(f'{status_path_stats} status check')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
 
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
 
-if __name__ == '__main__':
-    print_nginx_stats()
+    print(f'{status_check} status check')
